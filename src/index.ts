cea5bba5091e66f491070746f2b391606afb642b
Upender Kalwa <upender000@gmail.com>
2020-08-10 01:30:35 +0000

    Add docker ip utilities

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
 import { readFileSync } from "fs";
+import dns from "dns";
+import { promisify } from "util";
+import cp from "child_process";
+
+export const lookup = promisify(dns.lookup);
+export const exec = promisify(cp.exec);
 
 // https://stackoverflow.com/a/37015387
-export function isInDocker(): boolean {
+export const isInDocker = (): boolean => {
   const file = readFileSync("/proc/self/cgroup", "utf-8");
   return file.indexOf("/docker") !== -1;
+};
+
+export function validateIsInDocker(): void {
+  if (!isInDocker()) {
+    throw new Error(
+      "Attempted to call the method from outside docker container!"
+    );
+  }
+}
+
+export const lookupUpAddress = async (host: string): Promise<string> => {
+  const result = await lookup(host, {
+    family: 4,
+    hints: dns.ADDRCONFIG | dns.V4MAPPED,
+  });
+  return result.address;
+};
+
+export async function getContainerIp(): Promise<string> {
+  validateIsInDocker();
+  return (await exec("hostname -i")).stdout.trim();
+}
+
+export async function getHostIp(): Promise<string> {
+  validateIsInDocker();
+  return await lookupUpAddress("host.docker.internal");
+}
+
+export async function getGatewayIp(): Promise<string> {
+  validateIsInDocker();
+  return await lookupUpAddress("gateway.docker.internal");
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,23 +1,71 @@
 import fs from "fs";
-import { isInDocker } from "../src";
+import * as dockerIpTools from "../src";
 
 describe("isInDocker", () => {
   const mock = jest.spyOn(fs, "readFileSync");
   beforeEach(() => {
     mock.mockReset();
   });
+  afterAll(() => {
+    mock.mockRestore();
+  });
   test("returns true if inside docker", () => {
     mock.mockReturnValueOnce(
       "2:cpu:/docker/7e5b8976101572b85e0aeea155d3350ce2f3fbaf068bd894c094977a004002ac\n" +
         "1:cpuset:/docker/7e5b8976101572b85e0aeea155d3350ce2f3fbaf068bd894c094977a004002ac\n"
     );
-    expect(isInDocker()).toBe(true);
+    expect(dockerIpTools.isInDocker()).toBe(true);
     expect(mock).toBeCalledTimes(1);
   });
 
   test("returns false if inside docker", () => {
     mock.mockReturnValueOnce("2:cpu:/\n" + "1:cpuset:/\n");
-    expect(isInDocker()).toBe(false);
+    expect(dockerIpTools.isInDocker()).toBe(false);
     expect(mock).toBeCalledTimes(1);
   });
+});
+
+test("validates if the module is called from inside container or not", () => {
+  const mock = jest
+    .spyOn(dockerIpTools, "isInDocker")
+    .mockReturnValueOnce(true)
+    .mockReturnValueOnce(false);
+  expect(dockerIpTools.validateIsInDocker()).toBe(undefined);
+  expect(() => dockerIpTools.validateIsInDocker()).toThrow();
+  expect(mock).toBeCalledTimes(2);
+
+  mock.mockRestore();
+});
+
+test("methods throw errors if they are executed outside container", async () => {
+  const mock = jest.spyOn(dockerIpTools, "isInDocker").mockReturnValue(false);
+  await expect(() => dockerIpTools.getContainerIp()).rejects.toEqual(
+    new Error("Attempted to call the method from outside docker container!")
+  );
+  await expect(() => dockerIpTools.getGatewayIp()).rejects.toEqual(
+    new Error("Attempted to call the method from outside docker container!")
+  );
+  await expect(() => dockerIpTools.getHostIp()).rejects.toEqual(
+    new Error("Attempted to call the method from outside docker container!")
+  );
+
+  mock.mockRestore();
+});
+
+test("methods return IP addresses", async () => {
+  const mock = jest.spyOn(dockerIpTools, "isInDocker").mockReturnValue(true);
+  const mock2 = jest
+    .spyOn(dockerIpTools, "exec")
+    .mockResolvedValueOnce({ stdout: "test-ip", stderr: "error" });
+  const lookupSpy = jest
+    .spyOn(dockerIpTools, "lookup")
+    .mockResolvedValueOnce({ address: "1.2.3.4", family: 4 })
+    .mockResolvedValueOnce({ address: "5.6.7.8", family: 4 });
+  await expect(dockerIpTools.getContainerIp()).resolves.toEqual("test-ip");
+  await expect(dockerIpTools.getGatewayIp()).resolves.toEqual("1.2.3.4");
+  await expect(dockerIpTools.getHostIp()).resolves.toEqual("5.6.7.8");
+
+  mock.mockRestore();
+  mock2.mockRestore();
+  lookupSpy.mockRestore();
 });
